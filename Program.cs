daa331a991de1948051580f85ebd601b1dc423c7
Frederico <fred.mi@outlook.com>
2024-04-29 13:51:19 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿namespace AsyncTaskExample
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            static async Task<string> funcF6Async()
+            {
+                Console.WriteLine("Start f6");
+                await Task.Delay(6000);
+                Console.WriteLine("End f6");
+                return "Result f6";
+            }
+
+            static async Task<string> funcF2Async()
+            {
+                Console.WriteLine("Start f2");
+                await Task.Delay(2000);
+                Console.WriteLine("End f2");
+                return "Result f2";
+            }
+            static async Task<string> funcF4Async()
+            {
+                Console.WriteLine("Start f4");
+                await Task.Delay(4000);
+                Console.WriteLine("End f4");
+                return "Result f4";
+            }
+
+            List<Task<string>> functionList = [];
+
+            Task<string> f6 = funcF6Async();
+            functionList.Add(f6);
+
+            Task<string> f2 = funcF2Async();
+            functionList.Add(f2);
+
+            Task<string> f4 = funcF4Async();
+            functionList.Add(f4);
+
+            await Task.WhenAll(functionList);
+
+            foreach (Task<string> funcResult in functionList)
+            {
+                Console.WriteLine(funcResult.Result);
+            }
+
+        }
+    }
+}
